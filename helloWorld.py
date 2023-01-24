a7239e7f4f9c94735e2fcf57cc953709e785d8ba
ThomasMusselwhite <thomasmusselwhite1@gmail.com>
2023-01-24 17:50:33 +0000

    Added goodbye message

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,11 @@
 
 name = input("What is your name? ")
 
-print(f"Hello {name}")
+print(f"Hello {namef}")
 
 
 age = input("How old are you? ")
 
 print(f"You are {age} years old")
+
+print("Okay, goodbye")
